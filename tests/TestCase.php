27ea375712e240567ccd5705363db5ccd342bdc4
Tererai Mugova <tereraim@actionstarter.co.uk>
2021-10-20 15:33:42 +0200

    Initial Commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,17 @@
 
 namespace Tjmugova\LaravelFlash\Tests;
 
+use Tjmugova\LaravelFlash\FlashNotifier;
 use Tjmugova\LaravelFlash\LaravelFlashServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
+
+    protected $session;
+
+    protected $flash;
+
     /**
      * Setup the test environment.
      *
@@ -15,18 +21,37 @@ abstract class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
+        $this->session = \Mockery::spy('Laracasts\Flash\SessionStore');
+
+        $this->flash = new FlashNotifier($this->session);
     }
 
     /**
      * Get package providers.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param \Illuminate\Foundation\Application $app
      *
      * @return array
      */
     protected function getPackageProviders($app)
     {
         return [LaravelFlashServiceProvider::class];
+    }
+
+    /** @test */
+    public function view_has_message_using_short_helper()
+    {
+        flash('Saved');
+
+        $this->assertStringContainsString('Saved', view('flash::message')->render());
+    }
+
+    protected function assertSessionIsFlashed($times = 1)
+    {
+        $this->session
+            ->shouldHaveReceived('flash')
+            ->with('flash_notification', $this->flash->messages)
+            ->times($times);
     }
 
     public function notificationTypes(): array
